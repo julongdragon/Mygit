9f460435013390c105e6b4762ea07ebd24a0c161
Eakkasit tunsakool <julongdragon@gmail.com>
2016-08-11 00:19:43 +0700

    init เริ่มต้นแบบสวยๆ

--- Mygit/ViewController.swift
+++ Mygit/ViewController.swift
@@ -13,13 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //test 1
+        
     }
-
+// test 2
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+// test 3
+    
+    
+    
+    
+    
+    // test 4
 }
 
